81da41afce7bf575c7963410d6aca8b1be9b72da
Yoav <yoav.benzvi@gmail.com>
2020-04-28 11:05:23 +0200

    minor astethic changes to input

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,20 +3,34 @@ import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import Feed from '../components/Feed/Feed';
 
-const Home = () => {
-
 	const renderInputField = ({ input }) => {
 		return(
-			<input 
-				{...input}
-				className='border border-black' 
-				type='text' 
-				placeholder='write'/>
+			<div>
+				<div className='flex justify-center'>
+					<textarea 
+						{...input}
+						rows='4'
+						className='w-1/2 self-center border-2 border-md border-gray-600 bg-white text-center h-10 px-5 pr-16  text-sm focus:outline-none' 
+						type='textarea' 
+						placeholder="Write a new post..." 
+					/>
+				</div>
+				<div className='flex justify-center'>
+					<button 
+						className='m-2 w-1/6 rounded shadow-md shadow bg-blue-500 text-white hover:bg-blue-400'
+						type='submit  '
+					>
+						Post
+					</button>
+				</div>
+			</div>
 		)
 	}
 
+const Home = () => {
+
 	return(
-		<div className='w-full flex justify-center'>
+		<div className='w-full flex justify-center p-2'>
 			<div className='w-full flex flex-col'>
 				<Field name='content' component={renderInputField}/>
 				<Feed feed='home'/>

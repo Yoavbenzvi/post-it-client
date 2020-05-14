5ccfc25ce581694037bb0ec3df6c7ed9a069a09d
Yoav <yoav.benzvi@gmail.com>
2020-05-14 18:53:25 +0200

    heroku debugging

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,3 @@ import axios from 'axios';
 export default axios.create({
 	baseURL: 'https://post-it-app-backend.herokuapp.com/'
 });
-
-//https://git.heroku.com/post-it-app-backend.git
-//https://post-it-app-backend.herokuapp.com
-//https://addons-sso.heroku.com/robots.txt
